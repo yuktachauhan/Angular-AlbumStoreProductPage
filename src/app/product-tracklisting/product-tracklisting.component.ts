40da0e7afd6d7efc739bf49038e15cf09b6f4dc0
yuktachauhan <yuktach20167@gmail.com>
2021-02-11 15:15:07 +0530

    corrected

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,9 +9,9 @@ import { ProductService } from '../product.service'
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  constructor(private _productService : ProductService) { }
-
   albumInfo : Album;
+  
+  constructor(private _productService : ProductService) { }
 
   ngOnInit() {
      this._productService.getAlbum(1).subscribe(
