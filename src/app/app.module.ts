857b9c1b7f49a9be80809f38b192e4d4f26c00a3
yuktachauhan <yuktach20167@gmail.com>
2021-02-11 18:11:08 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule,  Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id' , component : ProductPageComponent},
+  { path: '' , redirectTo : 'products', pathMatch : 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +27,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
