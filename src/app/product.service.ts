03ff3d83196966cf7416fb9831cd3485437f3b83
yuktachauhan <yuktach20167@gmail.com>
2021-02-11 13:42:01 +0530

    created product service

---
+++ src/app/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http'
+import { Response } from '@angular/http'
+import 'rxjs/add/operator/map'
+
+@Injectable(
+
+)
+export class ProductService {
+
+  _albumUrl : string = "../assets/album.json";
+
+  constructor(private _http : Http) { }
+
+  getAlbum(id : number){
+    return this._http.get(_albumUrl).map(
+    (res : Response) => res.json());
+
+    //map here changes the response we get into json
+  }
+
+}
