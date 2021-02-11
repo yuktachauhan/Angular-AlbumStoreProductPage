8ef9fadd6aebe024d35c32d9fd0255a659e56d9a
yuktachauhan <yuktach20167@gmail.com>
2021-02-11 15:04:45 +0530

    created tracklisting component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ProductService } from './product.service'
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Album } from '../album'
+import { ProductService } from '../product.service'
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  constructor(private _productService : ProductService) { }
+
+  albumInfo : Album;
+
+  ngOnInit() {
+     this._productService.getAlbum(1).subscribe(
+       response => this.albumInfo = response;
+     );
+  }
+
+
+}
